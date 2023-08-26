3bb8f8774deff35148c0ac7b8d098e1cf5e9de99
Arman Kumar Jena <arman.k.jena07@gmail.com>
2023-08-26 10:15:41 +0530

    production setup

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ import "express-async-errors";
 import dotenv from "dotenv";
 dotenv.config();
 
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import path from "path";
+
 //ROUTES
 import authRouter from "./routes/authRoute.js";
 import notesRouter from "./routes/noteRoute.js";
@@ -17,13 +21,20 @@ import authentication from "./middleware/authentication.js";
 //ConnectDB
 import connectDB from "./db/connectDB.js";
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
+app.use(express.static(path.resolve(__dirname, "./client/dist")));
 app.use(cors());
 app.use(express.json());
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/notes", authentication, notesRouter);
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./client/dist", "index.html"));
+});
 
 app.use(errorMiddleware);
 app.use(notFoundMiddleware);
